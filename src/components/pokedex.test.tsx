b4d4067ccc16459b450e17b11fc8e775c7a59474
vivekb1702 <vivek.b@neoito.com>
2022-06-01 16:39:13 +0530

    changes in pokedex test cases

--- src/components/pokedex.test.tsx
+++ src/components/pokedex.test.tsx
@@ -18,22 +18,25 @@ afterEach(() => {
 
 describe("Pokedex Component Test", () => {
   it("Check Left button render", async () => {
-    await waitFor(() => expect(screen.getByText("Left")).toBeInTheDocument());
+    //  await waitFor(() => expect(screen.getByText("Left")).toBeInTheDocument());
+    expect(await screen.findByText("Left", {}, { timeout: 1000 }));
   });
   it("Check Right button render", async () => {
-    await waitFor(() => expect(screen.getByText("Right")).toBeInTheDocument());
+    //await waitFor(() => expect(screen.getByText("Right")).toBeInTheDocument());
+    expect(await screen.findByText("Right", {}, { timeout: 1000 }));
   });
   it("Check Pokemon Image render", async () => {
-    await waitFor(() => expect(screen.getByRole("img")).toBeInTheDocument());
+    expect(await screen.findByRole("img", {}, { timeout: 1000 }));
   });
   it("Check Pokemon Height Stat render", async () => {
-    await waitFor(() =>
-      expect(screen.getByText(/Height/i)).toBeInTheDocument()
-    );
+    // await waitFor(() =>
+    //   expect(screen.getByText(/Height/i)).toBeInTheDocument()
+    // );
+    expect(await screen.findByText(/Height/i, {}, { timeout: 1000 }));
   });
   it("Check Pokemon profile Change", async () => {
-    await waitFor(() => expect(screen.getByText(/Name/i)).toBeInTheDocument());
-    const button = screen.getByText("Right");
+    await screen.findByText(/Name/i, {}, { timeout: 1000 });
+    const button: HTMLButtonElement = screen.getByText("Right");
     const name: string = screen.getByText(/Name:/i).textContent!;
     act(() => {
       userEvent.click(button);
